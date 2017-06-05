94f777a3e6530dc05de3123f626c023270553e3a
Anielle <anielle.pizzo@enken.com.br>
2017-06-05 18:29:52 -0300

    mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
+$(function () {
+    //active botoes menu
 
+    var botao = $("nav.menu-topo ul li");
+
+    $(botao).click(function () {
+        $(this).addClass("active").siblings("li").removeClass("active");
+    });
+
+});

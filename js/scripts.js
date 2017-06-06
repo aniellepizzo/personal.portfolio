8279d632282e078af964ce6ed931c7bc421f6d21
Anielle <anielle.pizzo@enken.com.br>
2017-06-06 17:03:04 -0300

    link-active

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 $(function () {
     //active botoes menu
 
-    var botao = $("nav.menu-topo ul li");
+    var botao = $("nav.menu-topo ul li a");
 
-    $(botao).click(function () {
-        $(this).addClass("active").siblings("li").removeClass("active");
-    });
-
+    $(botao).click(function(e) {
+        var $this = $(this);
+        $this.parent().siblings().removeClass('active').end().addClass('active');
+        //e.preventDefault();
+    }); 
+    
+$(botao).filter(function() {
+    return this.href.pathname === window.location.pathname;
+).addClass("teste"); 
+    
 });
